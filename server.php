33135dd470af78f29c910382c3e339eae6befde3
Marten <you@example.com>
2019-10-08 06:02:46 +0900

    Test Api-1

--- server.php
+++ server.php
@@ -3,7 +3,7 @@
       $curl = curl_init();
       curl_setopt_array($curl, [
         CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => 'https://tnwebservices-test.ticketnetwork.com/tnwebservice/v3.2/tnwebservicestringinputs.asmx'
+        CURLOPT_URL => 'https://tnwebservices-test.ticketnetwork.com/tnwebservice/v3.2/tnwebservicestringinputs.asmx?op=GetCategories'
       ]);
       
       $resp = curl_exec($curl);
