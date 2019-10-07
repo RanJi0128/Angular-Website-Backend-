8c298b98b355053f478d6f9f8fc956d60f65aa52
Marten <you@example.com>
2019-10-08 05:37:14 +0900

    Test Api

---
+++ server.php
@@ -0,0 +1,13 @@
+<?php
+
+      $curl = curl_init();
+      curl_setopt_array($curl, [
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'https://tnwebservices-test.ticketnetwork.com/tnwebservice/v3.2/tnwebservicestringinputs.asmx'
+      ]);
+      
+      $resp = curl_exec($curl);
+      echo $resp;
+      curl_close($curl);
+  
+?>
