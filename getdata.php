4a157b4b559351ce1a837452eab898e8aa08ccc7
Marten <you@example.com>
2019-10-12 03:00:01 +0900

    GetEvent -6

--- getdata.php
+++ getdata.php
@@ -5,7 +5,7 @@
       curl_setopt_array($curl, [
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_URL => 'http://tnwebservices-test.ticketnetwork.com/tnwebservice/v3.2/tnwebservicestringinputs.asmx/SearchEvents?websiteConfigI
-        D=26432&searchTerms=U2&whereClause=city=%22Foxborough%22&orderByClause='
+        D=26432&searchTerms=U2'
       ]);
       
       $resp = curl_exec($curl);
