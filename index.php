e2061cbeef57e3f1ba013855cdc9423144595cb5
Marten <you@example.com>
2019-10-12 00:11:24 +0900

    GetEvent no test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
       $curl = curl_init();
       curl_setopt_array($curl, [
         CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => 'http://tnwebservices-test.ticketnetwork.com/tnwebservice/v3.2/tnwebservicestringinputs.asmx/'.$data.'?websiteConfigID=26432'
+        CURLOPT_URL => 'http://tnwebservices.ticketnetwork.com/tnwebservice/v3.2/tnwebservicestringinputs.asmx/'.$data.'?websiteConfigID=26432'
       ]);
       
       $resp = curl_exec($curl);
