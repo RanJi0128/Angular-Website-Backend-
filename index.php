8fd275983396d624325b501f8958c75577bb714c
Marten <you@example.com>
2019-10-11 11:17:37 +0900

    Get Data

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-      $category = $_GET['GetCategories'];
+      $data = $_GET['GetData'];
       $curl = curl_init();
       curl_setopt_array($curl, [
         CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => 'http://tnwebservices-test.ticketnetwork.com/tnwebservice/v3.2/tnwebservicestringinputs.asmx?websiteConfigID=26432'//.$category.'?websiteConfigID=26432'
+        CURLOPT_URL => 'http://tnwebservices-test.ticketnetwork.com/tnwebservice/v3.2/tnwebservicestringinputs.asmx'.$data.'?websiteConfigID=26432'
       ]);
       
       $resp = curl_exec($curl);
