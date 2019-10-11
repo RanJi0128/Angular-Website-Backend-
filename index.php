4a938bc096dd47793e9f275bf477a2b7bc46a910
Marten <you@example.com>
2019-10-11 11:06:03 +0900

    Basic url

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
       $curl = curl_init();
       curl_setopt_array($curl, [
         CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => 'http://tnwebservices-test.ticketnetwork.com/tnwebservice/v3.2/tnwebservicestringinputs.asmx/'.$category.'?websiteConfigID=26432'
+        CURLOPT_URL => 'http://tnwebservices-test.ticketnetwork.com/tnwebservice/v3.2/tnwebservicestringinputs.asmx?websiteConfigID=26432'//.$category.'?websiteConfigID=26432'
       ]);
       
       $resp = curl_exec($curl);
