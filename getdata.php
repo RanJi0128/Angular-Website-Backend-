7c05740ac6fe7b7b0f58650fef8408e8fde25a2c
Marten <you@example.com>
2019-10-12 03:58:50 +0900

    GetEvent -9

--- getdata.php
+++ getdata.php
@@ -9,6 +9,7 @@
       ]);
       
       $resp = curl_exec($curl);
+      echo "result=>";
       echo $resp;
       curl_close($curl);      
   //http://tnwebservices-test.ticketnetwork.com/tnwebservice/v3.2/tnwebservicestringinputs.asmx/'.$data.'?websiteConfigID=26432&numberOfEvents=1&eventID=20
